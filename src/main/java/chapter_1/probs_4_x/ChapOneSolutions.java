87206363ea965fc652dc3468db996325d6b516b8
sebrown0 <thebrownfamily@live.co.uk>
2022-06-21 17:53:27 +0200

    Excersises upto Chap1 - 18

--- src/main/java/chapter_1/probs_4_x/ChapOneSolutions.java
+++ src/main/java/chapter_1/probs_4_x/ChapOneSolutions.java
@@ -3,9 +3,15 @@
  */
 package chapter_1.probs_4_x;
 
+import  static java.util.Map.Entry.comparingByValue;
+
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.StringJoiner;
+import java.util.stream.Collectors;;
 
 /**
  * @author SteveBrown
@@ -112,37 +118,154 @@ public class ChapOneSolutions {
 	  }
 	}
 	
-//	private static void permuteAndPrint(String prefix, String str) {
-//		int n = str.length();
-//		
-//		if(n == 0) {
-//			System.out.println(prefix + " "); 	
-//		}else {
-//			for(int i = 0; i < n; i++) {
-//				permuteAndPrint(prefix + str.charAt(i), str.substring(i+1, n) + str.substring(0, n));
-//			}
-//		}
-//	}
+	// problem 11
+	public static boolean isPalindrome(String str) {		
+		return str.equals(new StringBuilder(str).reverse().toString());
+	}
+	public static boolean checkForPalindrome(int left, String str) {
+		int strLen = str.length();
+		
+		if(left >= (strLen / 2)) {
+			return true;
+		}else if(str.charAt(left) == str.charAt(strLen - left - 1)) {
+			checkForPalindrome(++left, str);
+		}
+		return false;
+	}
 	
-//	public static List<String> generateAllPermsOfString(String str){
-//		List<String> perms = new ArrayList<>();
-//		int strLen = str.length();
-////		char[] chars = str.toCharArray();
-//		char temp;
-//		
-//		perms.add(str);
-//		
-//		for(int idx = 0; idx < strLen; idx++) {
-//			char[] perm = str.toCharArray();
-//			for(int jdx = idx; jdx < strLen-1; jdx++) {
-//				temp = perm[jdx]; 
-//				perm[jdx] = perm[jdx+1];
-//				perm[jdx+1] = temp;
-//				perms.add(String.valueOf(perm));
-//			}
-//		}
-//		
-//		perms.forEach(System.out::println);
-//		return perms;
-//	}
+	// problem 12
+	public static String removeDuplicateChars(String str) {		
+		StringBuilder sb = new StringBuilder();
+		List<Character> chars = new ArrayList<>();
+		
+		for(char c: str.toCharArray()) {
+			if(chars.contains(c) == false) {
+				sb.append(c);
+				chars.add(c);
+			}
+		}
+		return sb.toString();
+	}
+	public static String removeDuplicateChars_Java8(String str) {		
+		return 
+			Arrays.asList(str.split("")).stream()
+				.distinct()
+				.collect(Collectors.joining());
+	}
+	
+	// problem 13
+	public static String removeGivenChars(String str, char... remove) {
+		StringBuilder sb = new StringBuilder();
+		List<Character> charsToRem = new ArrayList<>();
+		
+		for(char c : remove) {
+			charsToRem.add(c);
+		}
+		
+		for(char c : str.toCharArray()) {
+			if(charsToRem.contains(c) == false) {
+				sb.append(c);
+			}
+		}
+		return sb.toString();
+	}
+	public static String removeGivenChars_Java8(String str, char... remove) {		
+		List<Character> charsToRem = new ArrayList<>();
+		
+		for(char c : remove) {
+			charsToRem.add(c);
+		}
+		
+		return str.chars()
+				.filter(c -> charsToRem.contains((char)c) == false)			
+				.mapToObj(c -> String.valueOf((char)c))			
+				.collect(Collectors.joining());		
+	}
+	
+	// problem 14
+	public static char charThatAppearsTheMost(String str) {
+		char res = 0;
+		Map<Character, Integer> charCount = new HashMap<>();
+		int maxVal = -1;
+		
+		for(char c : str.toCharArray()) {			
+			charCount.compute(c, (k, v) -> (v == null) ? 1 : ++v);							
+		}
+		
+		for(Map.Entry<Character, Integer> e : charCount.entrySet()) {
+			if(e.getValue() > maxVal) {
+				res = e.getKey();
+				maxVal = e.getValue();
+			}
+		}
+
+		return res;
+	}	
+	public static char charThatAppearsTheMost_Java8(String str) {
+		return str.chars()
+			.filter(c -> !Character.isWhitespace(c))
+			.mapToObj(c -> (char) c)
+			.collect(Collectors.groupingBy(c -> c, Collectors.counting()))
+		    .entrySet()
+		    .stream()
+		    .max(comparingByValue())
+		    .map(p -> p.getKey())		    
+		    .orElse(Character.valueOf((char) 0));		
+	}
+	
+	// problem 15
+	public static void sortStringsByLen(String...strings) {
+		Map<Integer, List<String>> sorted = new HashMap<>();				
+		
+		for(String s: strings) {
+			Integer len = Integer.valueOf(s.length());
+			sorted.compute(len, 
+				(k,v) -> {
+					List<String> lst;
+					if(v == null) { 
+						lst = new ArrayList<String>();
+						lst.add(s);
+						return lst;
+					}else {
+						lst = sorted.get(len);
+						lst.add(s);						
+						return lst;
+					}});
+		}
+	}
+
+	// problem 16
+		public static boolean stringContainsSubStr(String str, String subStr) {		
+			return str.contains(subStr);
+		}
+		
+	// problem 17
+	public static int countOccursOfSubStringInStr(String str, String toFind) {
+		  int position = 0;
+		  int count = 0;
+		  int n = toFind.length();
+
+		  while ((position = str.indexOf(toFind, position)) != -1) {
+		    position = position + n;
+		    count++;
+		  }
+		  return count;
+	}
+	
+	// problem 18
+	public static boolean isAnagram(String word, String toFind) {
+		int idx;
+		StringBuilder sb = new StringBuilder(word.toLowerCase().replace(" ", ""));
+		
+		for(char c : toFind.toLowerCase().replace(" ", "").toCharArray()) {
+			idx = sb.indexOf(String.valueOf(c));
+			if(idx >= 0) {
+				sb.deleteCharAt(idx);
+			}else {
+				return false;
+			}
+		}
+		return true;
+	}
+	
 }
--- src/test/java/chapter_1/ChapterOne_Tests.java
+++ src/test/java/chapter_1/ChapterOne_Tests.java
@@ -131,4 +131,76 @@ class ChapterOne_Tests {
 	void prob10_allPerms() {
 		ChapOneSolutions.generateAllPermsOfString("abc");
 	}
+	
+	//Problem 11 - Test for palindrome.
+	@Test
+	void prob11_isPalindrome_true() {
+		assertTrue(ChapOneSolutions.isPalindrome("abba"));
+	}
+	@Test
+	void prob11_isPalindrome_false() {
+		assertTrue(ChapOneSolutions.isPalindrome("abbba"));
+	}
+	
+	//Problem 12 - Remove duplicate chars from string.
+	@Test
+	void prob12_removeDups() {
+		assertEquals("abcd", ChapOneSolutions.removeDuplicateChars("abbcccdd"));
+	}
+	@Test
+	void prob12_removeDups_Java8() {
+		assertEquals("abcd", ChapOneSolutions.removeDuplicateChars_Java8("abbcccdd"));
+	}
+	
+	//Problem 13 - Remove given characters.
+	@Test
+	void prob13_removeGivenChars() {
+		assertEquals("abcd", ChapOneSolutions.removeGivenChars("abXcd", 'X', 'y'));
+	}
+	@Test
+	void prob13_removeGivenChars_Java8() {
+		assertEquals("abcd", ChapOneSolutions.removeGivenChars_Java8("abXcd", 'X', 'y'));
+	}
+	
+	//Problem 14 - Find character with most occurrences.
+	@Test
+	void prob14_findCharWithMostOccurs() {
+		assertEquals('a', ChapOneSolutions.charThatAppearsTheMost("abXcadY"));
+	}
+	@Test
+	void prob14_findCharWithMostOccurs_Java8() {
+		assertEquals('a', ChapOneSolutions.charThatAppearsTheMost_Java8("abXcadY"));	
+	}
+	
+	//Problem 15 - Sort array of strings by length.
+	@Test
+	void prob15_sortStringsByLen() {
+		ChapOneSolutions.sortStringsByLen("a","ab","abc");
+	}
+	
+	//Problem 16 - Check if a string contains a sub string.
+	@Test
+	void prob16_strContains_true() {
+		assertTrue(ChapOneSolutions.stringContainsSubStr("abcdef", "abc"));
+	}
+	@Test
+	void prob16_strContains_false() {
+		assertFalse(ChapOneSolutions.stringContainsSubStr("abcdef", "abx"));
+	}
+	
+	//Problem 17 - Count occurrences of sub string in string.
+	@Test
+	void prob17_countOccursOfSubString() {
+		assertEquals(4, ChapOneSolutions.countOccursOfSubStringInStr("abcdabefabghab", "ab"));
+	}
+	
+	//Problem 18 - Is anagram.
+	@Test
+	void prob18_isAnangram_true() {
+		assertTrue(ChapOneSolutions.isAnagram("anagram", "nag a RAM"));
+	}
+	@Test
+	void prob18_isAnangram_false() {
+		assertFalse(ChapOneSolutions.isAnagram("anagram", "nagarams"));
+	}
 }
